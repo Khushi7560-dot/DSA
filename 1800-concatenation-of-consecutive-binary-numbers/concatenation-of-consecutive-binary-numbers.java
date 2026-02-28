9f98a8deea4054f000f90242904da84721740849
Khushi888 <khushi.24scse1011523@galgotiasuniversity.ac.in>
2026-02-28 23:53:06 +0530

    Time: 41 ms (50.00%) | Memory: 42.7 MB (50.00%) - LeetSync

---
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int concatenatedBinary(int n) {
+        long ans = 0;
+        int mod = 1000000007;
+        int length = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0) {
+                length++;
+            }
+
+            ans = ((ans << length) % mod + i) % mod;
+        }
+
+        return (int) ans;
+    }
+}
